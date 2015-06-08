547518b1e83c25ce376fa7c9c488ff2e821460c2
alexander <alexander.fedorov@rtlabs.ru>
2015-06-08 19:24:50 +0300

    Validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,18 @@
 class Card < ActiveRecord::Base
 
-  validates :original_text, presence: true
+  validates :original_text,   presence: true
+  validates :translated_text, presence: true
+  validate  :check_duplication
+
 
   before_create do
     set_default_date
+  end
+
+  def check_duplication
+    if original_text == translated_text
+      errors.add(:duplication, 'Значения полей дублируют друг друга.') 
+    end
   end
 
   def set_default_date
