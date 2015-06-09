bd1c345460889e2944c3852839e5295aa3fb469a
alexander <alexander.fedorov@rtlabs.ru>
2015-06-09 20:37:43 +0300

    Seeds.rb and Nokogiri fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,12 @@ require "open-uri"
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-doc = Nokogiri::HTML(open("http://sanstv.ru/english_words"), nil, "UTF-8")
-
+doc  = Nokogiri::HTML(open("http://sanstv.ru/english_words"), nil, "UTF-8")
+date = Date.today
 i = 2
-2.times do 
+25.times do 
   original_text_parsed   = doc.css("#table.table tbody tr td")[i].text.strip
   translated_text_parsed = doc.css("#table.table tbody tr td")[i + 2].text
-  date                   = Date.today
   i += 5
   Card.create(translated_text: translated_text_parsed, 
               original_text:   original_text_parsed, 
