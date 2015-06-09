7bcc12d49ec0c250b2da80ec70cb3bf8f88ec12f
alexander <alexander.fedorov@rtlabs.ru>
2015-06-09 20:36:18 +0300

    Seeds.rb and Nokogiri

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+doc = Nokogiri::HTML(open("http://sanstv.ru/english_words"), nil, "UTF-8")
+
+i = 2
+2.times do 
+  original_text_parsed   = doc.css("#table.table tbody tr td")[i].text.strip
+  translated_text_parsed = doc.css("#table.table tbody tr td")[i + 2].text
+  date                   = Date.today
+  i += 5
+  Card.create(translated_text: translated_text_parsed, 
+              original_text:   original_text_parsed, 
+              review_date:     date)
+end
