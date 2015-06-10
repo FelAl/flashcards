872e5aa08eb97da010e303a846b0d9c144e0d5b8
alexander <alexander.fedorov@rtlabs.ru>
2015-06-10 08:40:49 +0300

    code style fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# encoding: utf-8
 require "open-uri"
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 doc  = Nokogiri::HTML(open("http://sanstv.ru/english_words"), nil, "UTF-8")
 date = Date.today
