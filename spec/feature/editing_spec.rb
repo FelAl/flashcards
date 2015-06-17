892964cc34d5582baa2e0a9c3d9160012ff5fdc0
alexander <alexander.fedorov@rtlabs.ru>
2015-06-17 18:10:38 +0300

    hound fixes

--- spec/feature/editing_spec.rb
+++ spec/feature/editing_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 feature "Editing cards" do
   scenario "Try to edit a card" do
-    card = create(:card)
+    create(:card)
     visit root_path
 
     click_link "Карточки"
-    click_link "Редактировать", :match => :first
+    click_link "Редактировать", match: :first
 
     fill_in "card_original_text", with: "aba"
     fill_in "card_translated_text", with: "qwq"
