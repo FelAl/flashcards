5261e6fa067dacb114c705cae9c5b4a1df18bc30
alexander <alexander.fedorov@rtlabs.ru>
2015-06-24 19:39:33 +0300

    twitter begin

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -1,8 +1,19 @@
-Rails.application.config.sorcery.submodules = []
+Rails.application.config.sorcery.submodules = [:external]
 
 Rails.application.config.sorcery.configure do |config| 
+
+
+  config.external_providers = [:twitter]
+
+  config.twitter.key = "<your key here>"
+  config.twitter.secret = "<your key here>"
+  config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
+  config.twitter.user_info_mapping = {:username => "screen_name"}
   # --- user config ---
   config.user_config do |user|
+
+    # -- external --
+       user.authentications_class = Authentication
   end
   config.user_class = "User"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   get "login" => "user_sessions#new", as: :login
   delete "logout" => "user_sessions#destroy", as: :logout
+
+
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+
 
   get "/auth/twitter/callback", to: "user_sessions#create"
   resources :user_sessions
