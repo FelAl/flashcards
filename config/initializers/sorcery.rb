39dff003d55786935dbbe6c9037579feea431334
alexander <alexander.fedorov@rtlabs.ru>
2015-06-28 19:34:09 +0300

    fix prod url

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -8,6 +8,8 @@ Rails.application.config.sorcery.configure do |config|
   # config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
   if ENV["RAILS_ENV"] == "development" or ENV["RAILS_ENV"] == "test"
     app_url = "http://0.0.0.0:3000"
+  else
+    app_url = ""
   end
   config.twitter.callback_url = app_url + "/oauth/callback?provider=twitter"
   config.twitter.user_info_mapping = { email: "screen_name" }
@@ -17,6 +19,8 @@ Rails.application.config.sorcery.configure do |config|
   # config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
   if ENV["RAILS_ENV"] == "development" or ENV["RAILS_ENV"] == "test"
     app_url = "http://0.0.0.0:3000"
+  else
+    app_url = ""
   end
   config.facebook.callback_url = app_url + "/oauth/callback?provider=facebook"
   config.facebook.user_info_mapping = { email: "email" } 
