6fc9c60848e6b6db20a73b222dc6a2f02c01455b
alexander <alexander.fedorov@rtlabs.ru>
2015-06-28 09:56:19 +0300

    config fix

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -6,7 +6,7 @@ Rails.application.config.sorcery.configure do |config|
   config.external_providers = [:twitter]
 
   config.twitter.key = "t45chtlC5twRxh1v766Ec44gw"
-  config.twitter.secret = "VMKb2Imnvgdg2WIkYGpKaKBQTPRBzDfx1OqGjTC9l9SEzJN2fW>"
+  config.twitter.secret = "VMKb2Imnvgdg2WIkYGpKaKBQTPRBzDfx1OqGjTC9l9SEzJN2fW"
   config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
   config.twitter.user_info_mapping = {:username => "screen_name"}
   # --- user config ---
