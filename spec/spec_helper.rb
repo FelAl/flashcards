15fc06ba07965967889ecdfe507524e4b49a8115
alexander <alexander.fedorov@rtlabs.ru>
2015-06-28 16:07:21 +0300

    spaces

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,14 +8,14 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
-   config.around(:each) do |example|
-     DatabaseCleaner.cleaning do
-       example.run
-     end
-   end
-   config.after(:each) do
-     DatabaseCleaner.clean
-   end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
